f7b74a9d2c6fabd06cc9a1f1ff7b75b3ca4da639
Lip Cowan <pcowan90@gmail.com>
2020-10-07 08:48:04 -0700

    added some more methods to languageService

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,3 +1,5 @@
+const LinkedList = require('../LinkedList');
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
@@ -10,7 +12,7 @@ const LanguageService = {
         'language.total_score',
       )
       .where('language.user_id', user_id)
-      .first()
+      .first();
   },
 
   getLanguageWords(db, language_id) {
@@ -26,8 +28,66 @@ const LanguageService = {
         'correct_count',
         'incorrect_count',
       )
-      .where({ language_id })
+      .where({ language_id });
   },
-}
+
+  getWord(db, language_id, word_id) {
+    return db
+      .from('word')
+      .select(
+        'original',
+        'translation',
+        'next')
+      .where({language_id})
+      .andWhere({word_id});
+  },
+
+  getHeadWord(db, language_id, head) {
+    return db
+      .from('word')
+      .select(
+        'word.original',
+        'language.total_score',
+        'word.correct_count',
+        'word.incorrect_count'
+      )
+      .join('language', 'word.language_id', 'language.id')
+      .where('word.language_id', language_id)
+      .andWhere('word.id', head)
+      .first();
+  },
+
+  getAllWordsWithHead(db, language_id) {
+    return db
+      .from('word')
+      .select(
+        'language.head',
+        'word.id',
+        'word.language_id',
+        'word.original',
+        'word.translation',
+        'word.next',
+        'word.memory_value',
+        'word.correct_count',
+        'word.incorrect_count'
+      )
+      .join('language', 'word.language_id', 'language.id')
+      .where('word.language_id', language_id);
+  },
+
+  createWordsList(db, language_id) {
+    const wordList = new LinkedList();
+    return LanguageService.getAllWordsWithHead(db, language_id)
+      .then(wordArray => {
+        // start with creating headWord using insertFirst method
+        const headWord = wordArray.find(word => word.head === word.id);
+        wordList.insertFirst(headWord); 
+        // loop through and insert after word where currWord.next eqls word.id
+        // wordArray.forEach(word => wordList.insertLast(word))
+        return wordList;
+      });
+  },
+  
+};
 
 module.exports = LanguageService
