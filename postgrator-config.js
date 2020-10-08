a32d82e94e5fa9cd5348765a94fc69d2ed895346
Julia Bugayev <jbugayev18@gmail.com>
2020-10-07 22:43:47 -0700

    Updates

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  // "host": process.env.MIGRATION_DB_HOST,
-  // "port": process.env.MIGRATION_DB_PORT,
-  // "database": process.env.MIGRATION_DB_NAME,
-  // "username": process.env.MIGRATION_DB_USER,
-  // "password": process.env.MIGRATION_DB_PASS,
-  "connectionString": process.env.DATABASE_URL
+  "host": process.env.MIGRATION_DB_HOST,
+  "port": process.env.MIGRATION_DB_PORT,
+  "database": process.env.MIGRATION_DB_NAME,
+  "username": process.env.MIGRATION_DB_USER,
+  "password": process.env.MIGRATION_DB_PASS,
+  // "connectionString": process.env.DATABASE_URL
 }
